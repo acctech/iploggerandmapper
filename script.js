ec853827906e4dfe3c97c5875aa73000de77f7dd
accicttech1 <dannyfalero@acc.edu.au>
2020-12-05 00:38:57 +1100

    ASYNCgit add .! Faster

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();

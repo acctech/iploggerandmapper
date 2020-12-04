f8a65765f0a241baf7db51ee945da0ad35470bcc
accicttech1 <dannyfalero@acc.edu.au>
2020-12-05 00:38:30 +1100

    ASYNCgit add .! faster

--- script.js
+++ script.js
@@ -6,24 +6,44 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-function getIP(){
-    return JSON.parse(httpGet("https://api.ipify.org/?format=json")).ip;
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
+    xmlHttp.onload = function() {
+        callback(xmlHttp.responseText);
+    }
+    xmlHttp.send( null );
 }
 
-function logMyDownload(pdfURL, counterFor){
-    let ip = getIP();
-    let responseBody = JSON.parse(httpGet("https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec" + "?ip=" + ip + "&counterFor=" + counterFor)).body;
+function getIP(callback){
+    httpGetAsync("https://api.ipify.org/?format=json", function(response){
+        let ip = JSON.parse(response).ip;
+        callback(ip);
+    });
+}
+
+function logMyDownload(pdfURL, counterFor, callback){
+    try{
+        getIP(function(ip){
+            httpGetAsync("https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec" + "?ip=" + ip + "&counterFor=" + counterFor, callback);
+            console.log("Thank you for downloading.");
+        });
+    } catch (e) {}
     // Let the user get the download here //
     window.open(pdfURL, "_blank");
 }
 
-function getDownloadCounter(counterFor){
+function getDownloadCounter(counterFor, callback){
     //https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec?geodata=1&counter=1&counterFor=academia
-    let responseBody = JSON.parse(httpGet("https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec" + "?geodata=1&counter=1" + "&counterFor=" + counterFor));
-    return(Math.trunc(responseBody.counter));
+    httpGetAsync("https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec" + "?geodata=1&counter=1" + "&counterFor=" + counterFor, function(response){
+        let responseBody = JSON.parse(response);
+        let integerCounter = Math.trunc(responseBody.counter);
+        callback(integerCounter);
+    });
 }
 
-function getGoogleGeoData(locationDataFor){
-   return JSON.parse(httpGet("https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec" + "?geodata=1&googlemap=1&locationDataFor=" + locationDataFor));
+function getGoogleGeoData(locationDataFor, callback){
+   httpGetAsync("https://script.google.com/macros/s/AKfycbxMeewqkNbSxvWV-XkEpma5ATDTiA5_rd78xdGvMZwGa1NkNJnI/exec" + "?geodata=1&googlemap=1&locationDataFor=" + locationDataFor, callback);
 }
 
